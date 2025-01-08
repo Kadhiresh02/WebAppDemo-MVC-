7291e06ff9094fd066d9c60a430a01b8a8e4bee6
kadhiresh.s <kadhiresh02@gmail.com>
2025-01-08 18:40:19 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppTutorials.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
